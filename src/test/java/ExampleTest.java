d01f111ac3853f6a6392bf95d4f712267407382e
vibhor saxena <vibhor.saxena@genpact.com>
2021-08-12 18:23:49 +0530

    updated

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ExampleTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

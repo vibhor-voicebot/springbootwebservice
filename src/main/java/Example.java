6f37160363d0299f6394e3a68ac2e58da80d9ed8
Vibhor Saxena <vibhorsaxena2@gmail.com>
2021-09-15 12:17:51 +0000

    AB#248

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,15 +1,27 @@
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @RestController
 @EnableAutoConfiguration
 public class Example {
 
-    @RequestMapping("/index")
-    public String home() {
-        return "Context root-path for springboot rest controller!";
+    @RequestMapping(path = "/index", produces = "application/json; charset=UTF-8" )
+    public String home() throws JSONException  {
+    	JSONObject entity = new JSONObject();
+    	entity.put("ContextRoot", "/index");
+    	//HashMap<String, String> objMap = new HashMap<>();
+    	Gson gson = new Gson();
+        String json = gson.toJson(entity);
+        System.out.println(json);
+        //return "Context root-path for springboot rest controller!";
+    	//return entity;
+        return json;
     }
 
     public static void main(String[] args) throws Exception {
